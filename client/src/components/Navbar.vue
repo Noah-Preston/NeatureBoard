fe42d5dc7802f46498b1ed051daab7824335a21c
Kristina Todd <Kristina.Todd22@gmail.com>
2020-03-02 14:46:23 -0700

    navbar/homepage/boards view styling

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,31 +1,16 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand" :to="{ name: 'home' }">Kanban</router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+  <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid">
+    <router-link class="navbar-brand " :to="{ name: 'boards' }">
+      <img src="../assets/logo.png" class="logo" />
+      <h5 class="logoFont"><strong>NeatureBoard</strong></h5>
+    </router-link>
+    <div class="col-6 col-md-10 justify-content-center d-flex mx-auto logoFont neat"> "That's pretty neat!"</div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item" :class="{ active: $route.name == 'home' }">
-          <router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
-        </li>
-        <li
-          class="nav-item"
-          v-if="$auth.isAuthenticated"
-          :class="{ active: $route.name == 'boards' }"
-        >
-          <router-link class="nav-link" :to="{ name: 'boards' }">My-Dashboard</router-link>
-        </li>
-      </ul>
-      <span class="navbar-text">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+      <span class="navbar-text ">
         <button class="btn btn-success" @click="login" v-if="!$auth.isAuthenticated">Login</button>
         <button class="btn btn-danger" @click="logout" v-else>logout</button>
       </span>
@@ -34,29 +19,35 @@
 </template>
 
 <script>
-import axios from "axios";
+  import axios from "axios";
 
-let _api = axios.create({
-  baseURL: "https://localhost:3000",
-  withCredentials: true
-});
-export default {
-  name: "Navbar",
-  methods: {
-    async login() {
-      await this.$auth.loginWithPopup();
-      this.$store.dispatch("setBearer", this.$auth.bearer);
-      this.$store.dispatch("getProfile");
-      console.log("this.$auth.user: ");
-      console.log(this.$auth.user);
-    },
-    async logout() {
-      await this.$auth.logout();
-      this.$store.dispatch("resetBearer");
-      this.$router.push({ name: "home" });
+  let _api = axios.create({
+    baseURL: "https://localhost:3000",
+    withCredentials: true
+  });
+  export default {
+    name: "Navbar",
+    methods: {
+      async logout() {
+        await this.$auth.logout();
+        this.$store.dispatch("resetBearer");
+        this.$router.push({ name: "home" });
+      }
     }
-  }
-};
+  };
 </script>
 
-<style></style>
+<style>
+  .logo {
+    max-width: 4vw;
+  }
+
+  .logoFont {
+    font-family: 'Amatic SC';
+  }
+
+  .neat {
+    font-size: 6vh;
+    font-weight: 800;
+  }
+</style>
