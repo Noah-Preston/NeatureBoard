2239fe367c4e492103f91e56b1ca5506fafa227f
Noah-Preston <noah.preston2@gmail.com>
2020-03-03 16:31:06 -0700

    features and such

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,16 +1,27 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid">
-    <router-link class="navbar-brand " :to="{ name: 'boards' }">
+  <nav id="mb" class="mb navbar navbar-expand-lg navbar-light bg-light container-fluid">
+    <router-link class="navbar-brand" :to="{ name: 'boards' }">
       <img src="../assets/logo.png" class="logo" />
-      <h5 class="logoFont"><strong>NeatureBoard</strong></h5>
+      <h5 class="logoFont">
+        <strong>NeatureBoard</strong>
+      </h5>
     </router-link>
-    <div class="col-6 col-md-10 justify-content-center d-flex mx-auto logoFont neat"> "That's pretty neat!"</div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <div
+      class="col-6 col-md-10 justify-content-center d-flex mx-auto logoFont neat"
+    >"That's pretty neat!"</div>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarText">
-      <span class="navbar-text ">
+      <span class="navbar-text">
         <button class="btn btn-danger" @click="logout">logout</button>
       </span>
     </div>
@@ -18,35 +29,39 @@
 </template>
 
 <script>
-  import axios from "axios";
+import axios from "axios";
 
-  let _api = axios.create({
-    baseURL: "https://localhost:3000",
-    withCredentials: true
-  });
-  export default {
-    name: "Navbar",
-    methods: {
-      async logout() {
-        await this.$auth.logout();
-        this.$store.dispatch("resetBearer");
-        this.$router.push({ name: "home" });
-      }
+let _api = axios.create({
+  baseURL: "https://localhost:3000",
+  withCredentials: true
+});
+export default {
+  name: "Navbar",
+  methods: {
+    async logout() {
+      await this.$auth.logout();
+      this.$store.dispatch("resetBearer");
+      this.$router.push({ name: "home" });
     }
-  };
+  }
+};
 </script>
 
 <style>
-  .logo {
-    max-width: 4vw;
-  }
+.logo {
+  max-width: 4vw;
+}
 
-  .logoFont {
-    font-family: 'Amatic SC';
-  }
+.logoFont {
+  font-family: "Amatic SC";
+}
 
-  .neat {
-    font-size: 6vh;
-    font-weight: 800;
-  }
+.neat {
+  font-size: 6vh;
+  font-weight: 800;
+}
+#mb {
+  padding-bottom: 0;
+  padding-top: 0;
+}
 </style>
--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,70 +1,95 @@
 <template>
-  <div>
+  <div class="vw bg-img">
     <navbar />
-    <div class="board">
+    <div class="board text-light board-font">
       <h1 v-if="board.title">{{board.title}}</h1>
       <h1 v-else>Loading...</h1>
       <h3>{{board.description}}</h3>
     </div>
     <form @submit.prevent="addList" class="p-3">
       <input type="text" placeholder="title" v-model="newList.title" required class="rounded" />
-      <button type="submit" class="btn btn-success">Create List</button>
+      <button type="submit" class="btn btn-info">Create List</button>
     </form>
-    <div class="row">
-      <list v-for="(listObj, boardId) in lists" :key="listObj.id" :listData="listObj" :listIndex="index" />
+    <div class="row list-area">
+      <list
+        v-for="(listObj, index) in lists"
+        :key="listObj.id"
+        :listData="listObj"
+        :listIndex="index"
+      />
     </div>
   </div>
 </template>
 
 <script>
-  import Navbar from "../components/Navbar";
-  import List from "../components/List";
+import Navbar from "../components/Navbar";
+import List from "../components/List";
 
-  export default {
-    data() {
-      return {
-        newList: {
-          boardId: this.$route.params.boardId
-        }
-      };
-    },
-    mounted() {
-
-      this.$store.dispatch(
-        "getBoardById", {
-        id: this.$route.params.boardId
+export default {
+  data() {
+    return {
+      newList: {
+        boardId: this.$route.params.boardId
       }
-      )
-
+    };
+  },
+  mounted() {
+    this.$store.dispatch("getBoardById", {
+      id: this.$route.params.boardId
+    });
+    this.$store.dispatch("getListsByBoardId", {
+      id: this.$route.params.boardId
+    });
+  },
+  name: "board",
+  computed: {
+    board() {
+      return this.$store.state.activeBoard;
     },
-    name: "board",
-    computed: {
-      board() {
-        return this.$store.state.activeBoard;
-      },
-      lists() {
-        let data = this.$store.state.lists;
-        return data;
-      }
-    },
-    props: ["boardId"],
-    components: {
-      Navbar,
-      List
-    },
-    methods: {
-      addList() {
-        this.$store.dispatch("addList", this.newList);
-        this.newList = { title: "" };
-      },
-      addList() {
-        let data = {
-          boardId: this.$route.params.boardId,
-          title: this.newList.title
-        };
-        this.$store.dispatch("addList", data);
-      }
-
+    lists() {
+      let data = this.$store.state.lists;
+      return data;
     }
-  };
+  },
+  props: ["boardId"],
+  components: {
+    Navbar,
+    List
+  },
+  methods: {
+    addList() {
+      this.$store.dispatch("addList", this.newList);
+      this.newList = { title: "" };
+    },
+    addList() {
+      let data = {
+        boardId: this.$route.params.boardId,
+        title: this.newList.title
+      };
+      this.$store.dispatch("addList", data);
+    }
+  }
+};
 </script>
+
+<style scoped>
+.list-area {
+  overflow-x: auto;
+  height: 62.3vh;
+  white-space: nowrap;
+  margin-bottom: 0vh;
+  flex-wrap: nowrap;
+  width: 100vw;
+  margin-left: 0;
+}
+.vw {
+  max-width: 100%;
+}
+.board-font {
+  font-family: "Boogaloo", cursive;
+}
+.bg-img {
+  background-image: url("../assets/bark.jpeg");
+  background-size: cover;
+}
+</style>
