4834f8272ed2e73afd88d81033fc420fafc37cb8
Noah-Preston <noah.preston2@gmail.com>
2020-03-06 17:05:08 -0700

    fixin harder

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -41,7 +41,7 @@ export default {
   methods: {
     async logout() {
       if (await NotificationService.confirmLog()) {
-        await this.$auth.logout({ returnTo: "/" });
+        await this.$auth.logout({ returnTo: nwindow.location.href });
         this.$store.dispatch("resetBearer");
         this.$router.push({ name: "home" });
       }
